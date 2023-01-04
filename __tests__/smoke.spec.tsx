96d5dea5613b4c16fa1e7cea27b50b53f3e4d916
mortadha fadhlaoui <mortadhafadhlaoui@gmail.com>
2023-01-04 02:20:27 +0100

    add test coverage cmd

--- __tests__/smoke.spec.tsx
+++ __tests__/smoke.spec.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import renderer from 'react-test-renderer'
 
-import App from '../App';
-import { Header } from '../src';
+import App from '../App'
+import { Header } from '../src'
 
 describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    // @ts-ignore
-    expect(tree.children.length).toBe(1);
-  });
-});
+	it('has 2 child', () => {
+		const tree = renderer.create(<App />).toJSON()
+		// @ts-ignore
+		expect(tree.children.length).toBe(2)
+	})
+})
 
 describe('<Header />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<Header />).toJSON();
-    // @ts-ignore
-    expect(tree.children.length).toBe(1);
-  });
-});
+	it('has 1 child', () => {
+		const tree = renderer.create(<Header />).toJSON()
+		// @ts-ignore
+		expect(tree.children.length).toBe(1)
+	})
+})
