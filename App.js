be11c23ec237bc1c024c9ebf9c386a642d6afbae
Mortadha Fadhlaoui <mortadhafadhlaoui@gmail.com>
2023-01-30 17:36:43 +0100

    add react-native-paper

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import 'react-native-gesture-handler'
 import * as React from 'react'
+import { Provider as PaperProvider } from 'react-native-paper'
 import { RootNavigator } from './src/navigation/root-navigation'
 
 export default function App() {
-	return <RootNavigator />
+	return (
+		<PaperProvider>
+			<RootNavigator />
+		</PaperProvider>
+	)
 }
